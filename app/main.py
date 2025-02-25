89de01c35ab86595a33e0f5b59e66a4220176e38
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-02-25 21:50:28 +0100

    feat(cors): add temporary cors wildcard

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.api import routers
 from app.db.database import create_database_tables
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routers.chat_router, prefix="/chat", tags=["chat"])
 
